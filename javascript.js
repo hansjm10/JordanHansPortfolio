c4d7deca69b85975ce448829800bddd7a162f2b1
Jordan Hans <jordan.hans@ymail.com>
2021-12-23 14:22:29 -0600

    Added Adopt Pets Wisconsin

--- javascript.js
+++ javascript.js
@@ -1,7 +1,9 @@
 var slideIndex1 = 1;
 var slideIndex2 = 1;
+var slideIndex3 = 1;
 showSlides(slideIndex1,"one");
 showSlides(slideIndex2,"two");
+showSlides(slideIndex3, "three")
 // Next/previous controls
 function plusSlides1(n) {
   showSlides(slideIndex1 += n, "one");
@@ -9,6 +11,11 @@ function plusSlides1(n) {
 function plusSlides2(n) {
     showSlides(slideIndex2 += n, "two");
   }
+  function plusSlides3(n) {
+    showSlides(slideIndex3 += n, "three");
+  }
+
+
 
 function showSlides(n, slide) {
   var i;
@@ -19,9 +26,13 @@ function showSlides(n, slide) {
     console.log("True");
     var slides = document.getElementsByClassName("mySlides1");
   }
-  else
+  else if(slide ==="two")
   {
     var slides = document.getElementsByClassName("mySlides2");
+  }
+  else {
+    var slides = document.getElementsByClassName("mySlides3");
+
   }
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
@@ -33,7 +44,11 @@ function showSlides(n, slide) {
   if(slide === "one"){
   slideIndex1 = slideIndex
   }
-  else{
+  else if(slide === "two"){
       slideIndex2 = slideIndex
+  }
+  else
+  {
+    slideIndex3 = slideIndex
   }
 } 
